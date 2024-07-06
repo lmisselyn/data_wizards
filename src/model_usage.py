72e38b9806efd774b9b7eaf28d06c6ffa471ffb9
lmisselyn <45001744+lmisselyn@users.noreply.github.com>
2024-07-06 17:22:10 +0200

    [Edit] Edit model usage to handle the case when the model is not trained yet

--- src/model_usage.py
+++ src/model_usage.py
@@ -1,43 +1,24 @@
-import pandas as pd
-from sklearn.metrics import f1_score
-from sklearn.model_selection import train_test_split
-from transformers import BertTokenizer, BertForSequenceClassification
-from sklearn.feature_extraction.text import TfidfVectorizer
-import torch
-import nltk
-import string
-from data_preprocess import *
+import os
+from src.data_preprocess import *
+from src.NLP_classifier import generate_NB_model
 import pickle
 
-def transformer_classify_sentence(sentence):
-    model = BertForSequenceClassification.from_pretrained('./results/checkpoint-5364')
-    tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
-    model.eval()
-
-    sentence = ' '.join(tokenization_and_filtering_sentence(sentence))
-    inputs = tokenizer(sentence, return_tensors='pt', truncation=True, padding=True, max_length=128)
-
-    with torch.no_grad():
-        outputs = model(**inputs)
-
-    predictions = torch.argmax(outputs.logits, dim=1)
-    return predictions.item()
-
 def NB_classify_sentence(sentence):
+    """
+    Args:
+        sentence (string): the job description to classify
+    Returns:
+        string: if the description is fraudulent or not
+    """
+    if not os.path.exists('results/NB_classifier'):
+        generate_NB_model()
     model = pickle.load(open('results/NB_classifier', 'rb'))
     vectorizer = pickle.load(open('results/NB_vectorizer', 'rb'))
     tokens = tokenization_and_filtering_sentence(sentence)
     s = vectorizer.transform([' '.join(tokens)])
-    return model.predict(s)[0]
+    result = model.predict(s)[0]
+    if result == 0:
+        return "The job description is safe"
+    return "The job description is fraudulent"
 
 
-if __name__ == "__main__":
-    # df = pd.read_csv("Datasets/cleaned_dataset.csv")
-    # X_train, X_test, y_train, y_test = train_test_split(df['text'], df['label'], test_size=0.2, stratify=df['label'], random_state=42)
-    # y_pred = []
-    # for sentence in X_test:
-    #     y_pred.append(transformer_classify_sentence(sentence))
-    # f1 = f1_score(y_test, y_pred, average='binary')
-    # print(f"f1 score : {f1}")
-    print(transformer_classify_sentence('ok lq'))
-    print(NB_classify_sentence('ok lq'))
