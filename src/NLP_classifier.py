94399af6d322249606dc5f2e182fe71a3652ac47
lmisselyn <45001744+lmisselyn@users.noreply.github.com>
2024-07-08 10:51:54 +0200

    [Edit] Improve the model with 2-gram and a GridSearchCV to find the best smoothing parameter (alpha).

--- src/NLP_classifier.py
+++ src/NLP_classifier.py
@@ -1,9 +1,9 @@
 import os
 import pandas as pd
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import GridSearchCV, train_test_split
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.metrics import f1_score, accuracy_score
+from sklearn.metrics import f1_score, accuracy_score, precision_score, recall_score
 from imblearn.over_sampling import SMOTE
 import pickle
 from src.data_preprocess import *
@@ -14,7 +14,7 @@ def simple_NB_classifier(df):
         df (pd.Dataframe): the dataframe containing the labbelled job description
     Train a simple Naive Bayes classifier and store it in a pickle file
     """
-    vectorizer = TfidfVectorizer()
+    vectorizer = TfidfVectorizer(ngram_range=(1, 2))
     preprocessed_text = []
     tokens = tokenization_and_filtering_dataframe(df)
     for t in tokens:
@@ -33,18 +33,33 @@ def simple_NB_classifier(df):
     X_train, y_train= smote.fit_resample(X_train, y_train)
 
     model = MultinomialNB()
-    model.fit(X_train, y_train)
+    param_grid = {
+        'alpha': [0.1, 0.5, 1.0]
+    }
+    
+    grid_search = GridSearchCV(model, param_grid, scoring='f1', cv=5)
+    grid_search.fit(X_train, y_train)
+    model = grid_search.best_estimator_
+    
     pickle.dump(model, open('results/NB_classifier', 'wb'))
     y_pred = model.predict(X_test)
     
     accuracy = accuracy_score(y_test, y_pred)
     f1 = f1_score(y_test, y_pred, average='binary')
+    precision = precision_score(y_test, y_pred)
+    recall = recall_score(y_test, y_pred)
     
     print(f"Accuracy: {accuracy}")
     print(f"F1 score: {f1}")
+    print(f"Precision: {precision}")
+    print(f"Recall: {recall}")
     
 def generate_NB_model():
     filename = 'datasets/cleaned_dataset.csv'
     df = pd.read_csv(filename)
     simple_NB_classifier(df)
 
+
+if __name__=="__main__":
+    generate_NB_model()
+    
